1d2ad93380d043e1b696d2d82e7c38e0702128b1
ankoosh544 <63229690+ankoosh544@users.noreply.github.com>
2023-04-16 11:38:52 +0200

    authcontroller added with register login apis

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
    use HttpResponses;
 
+   public function login(){
+    return 'This is login Method';
+   }
+   public function register(StoreUserRequest $request){
+    $request->validated($request->all());
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' =>Hash::make($request->password),
+
+    ]);
+    return $this->success([
+        'user' => $user,
+        'token' => $user->createToken('API Token of '. $user->name)->plainTextToken
+    ]);
+   }
+   public function logout(){
+    return response()->json('This is Logout');
+   }
 }
